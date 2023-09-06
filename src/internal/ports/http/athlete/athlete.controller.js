7b714b96620e1d11de41db4dab90cbf4b53003d6
Luis Daniel Assulfi <lassulfi@gmail.com>
2023-09-05 21:40:36 -0300

    feat: :sparkles: create athlete http port

---
+++ src/internal/ports/http/athlete/athlete.controller.js
@@ -0,0 +1,47 @@
+export default class AthleteController {
+    #createAthleteUseCase
+    #getAthleteUseCase
+    #updateAthleteUseCase
+
+    constructor({createAthleteUseCase, getAthleteUseCase, updateAthleteUseCase}) {
+        this.#createAthleteUseCase = createAthleteUseCase
+        this.#getAthleteUseCase = getAthleteUseCase
+        this.#updateAthleteUseCase = updateAthleteUseCase
+    }
+
+    async create(request, response) {
+        const input = request.body
+
+        const output = await this.#createAthleteUseCase.execute(input)
+
+        return response.json(output)
+    }
+
+    async getById(request, response) {
+        try {
+            const { id } = request.params
+    
+            const output = await this.#getAthleteUseCase.execute({ id })
+    
+            return response.json(output)
+        } catch (err) {
+            return response.status(404).json({
+                error: err.message
+            })
+        }
+    }
+
+    async update(request, response) {
+        const { id } = request.params
+        const body = request.body
+
+        const input = {
+            id,
+            ...body
+        }
+
+        const output = await this.#updateAthleteUseCase.execute(input)
+
+        return response.json(output)
+    }
+}
---
+++ src/internal/ports/http/athlete/athlete.controller.test.js
@@ -0,0 +1,187 @@
+import { jest, describe, it, expect, beforeEach, afterEach } from "@jest/globals"
+import AthleteController from "./athlete.controller.js"
+
+const UseCaseMock = () => ({
+    execute: jest.fn()
+})
+
+let createAthleteUseCase, getAthleteUseCase, updateAthleteUseCase
+
+const mockResponse = {
+    json: jest.fn(),
+    status: jest.fn(function(code) {
+        this.statusCode = code
+        
+        return this
+    })
+}
+
+beforeEach(() => {
+    createAthleteUseCase = UseCaseMock()
+    getAthleteUseCase = UseCaseMock()
+    updateAthleteUseCase = UseCaseMock()
+})
+
+afterEach(() => {
+    jest.clearAllMocks()
+})
+
+describe("# Athlete controller unit tests", () => {
+    describe("# create method tests", () => {
+        it("should process request with success", async () => {
+            const controller = new AthleteController({
+                createAthleteUseCase,
+                getAthleteUseCase,
+                updateAthleteUseCase
+            })
+
+            const input =  {
+                name: "Athlete",
+                country: "Country",
+                birthday: {
+                    day: 1,
+                    month: 1,
+                    year: 2023
+                }
+            }
+
+            const output = {id: "123"}
+
+            const mockRequest = {
+                body: input
+            }
+
+            const createSpy = jest.spyOn(createAthleteUseCase, "execute").mockResolvedValue(output)
+            const getSpy = jest.spyOn(getAthleteUseCase, "execute")
+            const updateSpy = jest.spyOn(updateAthleteUseCase, "execute")
+
+            const responseJsonSpy = jest.spyOn(mockResponse, "json")
+
+            await controller.create(mockRequest, mockResponse)
+
+            expect(createSpy).toHaveBeenCalledWith(input)
+            expect(getSpy).not.toBeCalled()
+            expect(updateSpy).not.toBeCalled()
+            expect(responseJsonSpy).toHaveBeenCalledWith(output)
+        })
+    })
+
+    describe("# get by id unit tests", () => {
+        it("should retrieve athlete given a valid id", async () => {
+            const controller = new AthleteController({
+                createAthleteUseCase,
+                getAthleteUseCase,
+                updateAthleteUseCase
+            })
+
+            const input = { id: 123 }
+
+            const output = {
+                id: 123,
+                name: "Athlete",
+                country: "Country",
+                birthday: new Date(2023, 1, 1)
+            }
+
+            const mockRequest = {
+                params: input
+            }
+
+            const createSpy = jest.spyOn(createAthleteUseCase, "execute")
+            const getSpy = jest.spyOn(getAthleteUseCase, "execute").mockResolvedValue(output)
+            const updateSpy = jest.spyOn(updateAthleteUseCase, "execute")
+
+            const responseJsonSpy = jest.spyOn(mockResponse, "json")
+
+            await controller.getById(mockRequest, mockResponse)
+
+            expect(createSpy).not.toBeCalled()
+            expect(getSpy).toHaveBeenCalledWith(input)
+            expect(updateSpy).not.toBeCalled()
+            expect(responseJsonSpy).toHaveBeenCalledWith(output)
+        })
+
+        it("should not retrieve an athlete given an invalid id", async () => {
+            const controller = new AthleteController({
+                createAthleteUseCase,
+                getAthleteUseCase,
+                updateAthleteUseCase
+            })
+
+            const input = { id: 123 }
+
+            const mockRequest = {
+                params: input
+            }
+
+            const createSpy = jest.spyOn(createAthleteUseCase, "execute")
+            const getSpy = jest.spyOn(getAthleteUseCase, "execute").mockRejectedValue(new Error(`Error retrieving athlete with ID "${input.id}": Athlete not found`))
+            const updateSpy = jest.spyOn(updateAthleteUseCase, "execute")
+
+            const responseJsonSpy = jest.spyOn(mockResponse, "json")
+            const statusSpy = jest.spyOn(mockResponse, "status")
+
+            await controller.getById(mockRequest, mockResponse)
+
+            expect(createSpy).not.toBeCalled()
+            expect(getSpy).toHaveBeenCalledWith(input)
+            expect(updateSpy).not.toBeCalled()
+            expect(responseJsonSpy).toHaveBeenCalledWith({
+                error: `Error retrieving athlete with ID "${input.id}": Athlete not found`
+            })
+            expect(statusSpy).toHaveBeenCalledWith(404)
+        })
+    })
+
+    describe("# update method unit tests", () => {
+        it("should update an athlete with success", async () => {
+            const controller = new AthleteController({
+                createAthleteUseCase,
+                getAthleteUseCase,
+                updateAthleteUseCase
+            })
+
+            const id = "123"
+
+            const input =  {
+                name: "Athlete",
+                country: "Country",
+                birthday: {
+                    day: 1,
+                    month: 1,
+                    year: 2023
+                },
+                teamId: "234"
+            }
+
+            const output =  {
+                id,
+                name: "Athlete",
+                country: "Country",
+                birthday: new Date(2023, 1, 1),
+                team: {
+                    id: "234",
+                    name: "Team"
+                }
+            }
+
+            const mockRequest = {
+                params: {id},
+                body: input
+            }
+
+            const createSpy = jest.spyOn(createAthleteUseCase, "execute")
+            const getSpy = jest.spyOn(getAthleteUseCase, "execute")
+            const updateSpy = jest.spyOn(updateAthleteUseCase, "execute").mockResolvedValue(output)
+
+            const responseJsonSpy = jest.spyOn(mockResponse, "json")
+
+            await controller.update(mockRequest, mockResponse)
+
+            expect(createSpy).not.toBeCalled()
+            expect(getSpy).not.toBeCalled()
+            expect(updateSpy).toHaveBeenCalledWith({id, ...input})
+            expect(responseJsonSpy).toHaveBeenCalledWith(output)
+        })
+    })
+})
